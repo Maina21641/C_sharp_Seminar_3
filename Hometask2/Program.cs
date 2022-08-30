2fb0ffa0ec7772e1c4c44a8d3362649918b1ea56
Alex <Yulien.ru@Yandex.ru>
2022-08-30 13:59:47 +0400

    добавлены 2 задачи домашней работы

---
+++ Hometask2/Program.cs
@@ -0,0 +1,29 @@
+﻿// 1. Написать программу показывающуб вторую цифру трехзначного числа
+
+// int GetSecondDigit(int number){
+//     return number / 10 % 10;
+// }
+// Random rnd = new Random();
+// int n = 123;
+
+// // uint //unsigned // Math.Abs
+// System.Console.WriteLine($"Вторая цифра числа {n} - {GetSecondDigit(n)}");
+
+// 2. Написать программу вывода третьей цифру любого числа
+
+int GetSecondDigit(int number){
+    if (number < 100){
+        System.Console.WriteLine("Третьей цифры нет");
+        return 0;
+    }
+    int result = 0;
+    while (number > 999){ // пока число больше максимального трехзначного 
+        number /= 10; // делим на 10 пока разряды не упадут до трехзначного
+        result = number % 10; // выводим третий знак
+    }
+    return result;
+}
+
+System.Console.WriteLine(GetSecondDigit(5975613));
+
+
