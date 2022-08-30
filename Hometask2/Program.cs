bbba79dda9277b572ec9d9bfd4ea84d1396ade2c
Alex <Yulien.ru@Yandex.ru>
2022-08-31 03:51:33 +0400

    раздораны все 3 задачи домашенго задания

--- Hometask2/Program.cs
+++ Hometask2/Program.cs
@@ -11,19 +11,45 @@
 
 // 2. Написать программу вывода третьей цифру любого числа
 
-int GetSecondDigit(int number){
-    if (number < 100){
-        System.Console.WriteLine("Третьей цифры нет");
-        return 0;
+// int GetSecondDigit(int number){
+//     if (number < 100){
+//         System.Console.WriteLine("Третьей цифры нет");
+//         return 0;
+//     }
+//     int result = 0;
+//     while (number > 999){ // пока число больше максимального трехзначного 
+//         number /= 10; // делим на 10 пока разряды не упадут до трехзначного
+//         // number = number / 10; аналогична предыдущей строке
+//         result = number % 10; // выводим третий знак
+//     }
+//     return result;
+// }
+
+// System.Console.WriteLine(GetSecondDigit(5975613));
+
+// 2.1 Вариант вывода третьей цифры из кокретного числа с помощью массива
+
+// int GetSecondDigit(int number){
+//     int[] arr = new int[7];
+//     for (int i =0; number > 0; i ++){
+//         arr[i] = number % 10;
+//         number /=10;
+//     }
+//     return arr [arr.Length - 3];
+// }
+// System.Console.WriteLine(GetSecondDigit(5975613));
+
+// 3.  Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+bool IsWeekend(int number){
+    if (number > 7 || number < 1){
+        return false;
     }
-    int result = 0;
-    while (number > 999){ // пока число больше максимального трехзначного 
-        number /= 10; // делим на 10 пока разряды не упадут до трехзначного
-        result = number % 10; // выводим третий знак
+    if (number > 5){
+        return true;
     }
-    return result;
+    else{
+        return false;
+    }
 }
-
-System.Console.WriteLine(GetSecondDigit(5975613));
-
-
+Console.WriteLine(IsWeekend(9));
