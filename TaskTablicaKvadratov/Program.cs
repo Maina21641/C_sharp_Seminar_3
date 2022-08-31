68d292a3d563323ac94c6f42d173a7f15fa47ccc
Alex <Yulien.ru@Yandex.ru>
2022-08-31 07:02:59 +0400

    есть решение таблицы квадратов чисел от 1 до N

---
+++ TaskTablicaKvadratov/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+
+void GetPow (int number)
+{
+    for (int i = 1; i <= number; i ++)
+    {
+        System.Console.WriteLine(Math.Pow(i, 2));
+    }
+}
+GetPow(5);
